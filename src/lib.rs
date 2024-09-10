4a573e51d5eb99dc4348c80b65f75dc7e85d860b
KeeganBeuthin <kdbeuthin@gmail.com>
2024-09-10 13:38:35 +0200

    naming issue

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ mod http;
 // Re-export main functions for easier use
 pub use transactions::{execute_credit_leg, process_credit_result, execute_debit_leg};
 pub use query::generate_balance_query;
-pub use http::handle_http_request as custom_handle_http_request;
+pub use http::handle_http_request;
 
 #[no_mangle]
 pub extern "C" fn init() {
